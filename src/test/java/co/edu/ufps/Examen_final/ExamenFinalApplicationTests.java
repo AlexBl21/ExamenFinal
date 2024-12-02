5c498af475fade90cc9f4d1f58090ed762612785
AlexBl21 <hblancorolon@gmail.com>
2024-12-02 09:13:14 -0500

    plantilla

---
+++ src/test/java/co/edu/ufps/Examen_final/ExamenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.ufps.Examen_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
